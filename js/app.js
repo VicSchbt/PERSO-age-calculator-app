74e361efad559b1ad6117650e34aa4dfdab455b9
Victoire Schubert <victoire.schubert@gmail.com>
2024-04-21 21:18:55 +0200

    wip: error handling

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ const dayInput = document.querySelector('#calculator-date-form__day');
 const monthInput = document.querySelector('#calculator-date-form__month');
 const yearInput = document.querySelector('#calculator-date-form__year');
 
+const labels = document.querySelectorAll(
+	'.calculator-date-form__date-selectors div label'
+);
+
 const dayResult = document.querySelector('.calculator-result__days span');
 const monthResult = document.querySelector('.calculator-result__months span');
 const yearResult = document.querySelector('.calculator-result__years span');
@@ -17,29 +21,30 @@ submitBtn.addEventListener('click', (event) => {
 	const monthValue = monthInput.value;
 	const yearValue = yearInput.value;
 
-	const today = new Date();
-	const birthDate = new Date();
-	birthDate.setFullYear(yearValue);
-	birthDate.setMonth(monthValue - 1);
-	birthDate.setDate(dayValue);
+	if (dayValue === '') {
+		labels[0].classList.add('error');
+	} else {
+		const today = new Date();
+		const birthDate = new Date(yearValue, monthValue - 1, dayValue);
 
-	let years = today.getFullYear() - birthDate.getFullYear();
-	let months = today.getMonth() - birthDate.getMonth();
-	if (months < 0) {
-		years--;
-		months += 12;
-	}
-	let days = today.getDate() - birthDate.getDate();
-	if (days < 0) {
-		months--;
-		days += 30;
+		let years = today.getFullYear() - birthDate.getFullYear();
+		let months = today.getMonth() - birthDate.getMonth();
 		if (months < 0) {
 			years--;
 			months += 12;
 		}
-	}
+		let days = today.getDate() - birthDate.getDate();
+		if (days < 0) {
+			months--;
+			days += 30;
+			if (months < 0) {
+				years--;
+				months += 12;
+			}
+		}
 
-	dayResult.innerHTML = days;
-	monthResult.innerHTML = months;
-	yearResult.innerHTML = years;
+		dayResult.innerHTML = days;
+		monthResult.innerHTML = months;
+		yearResult.innerHTML = years;
+	}
 });
