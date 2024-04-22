64d8ce41a5a53835387c6a3146997f7e6ecfa968
VicSchbt <victoire.schubert@gmail.com>
2024-04-22 14:47:16 +0200

    fix: ci deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./", // Set the base path for assets
-  assetsInclude: ["assets/**", "css/**", "js/**"], // Include assets from the 'assets' folder
+  base: "./",
 });
