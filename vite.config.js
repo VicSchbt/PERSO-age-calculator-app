35fb9b9bd597f19e686672f27783d720dac68370
VicSchbt <victoire.schubert@gmail.com>
2024-04-22 14:25:46 +0200

    config: vite config

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "./", // Set the base path for assets
+  assetsInclude: ["assets/**", "css/**", "js/**"], // Include assets from the 'assets' folder
+  css: {
+    postcss: {},
+  },
+});
