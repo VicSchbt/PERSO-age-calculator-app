b9fd2cd79bf81a83a2fa1643baf50161e3fa60b1
VicSchbt <victoire.schubert@gmail.com>
2024-04-22 14:28:58 +0200

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,4 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "./", // Set the base path for assets
   assetsInclude: ["assets/**", "css/**", "js/**"], // Include assets from the 'assets' folder
-  css: {
-    postcss: {},
-  },
 });
